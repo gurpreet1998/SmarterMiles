e23d7d0a39f51a951cb0bcc3e1580ebe641151b7
Gurpreet Singh <gurpreet.singh.gs6@contractors.roche.com>
2023-11-11 18:58:00 -0800

    Added Rest of Files

---
+++ src/main/java/com/smartermiles/app/SmartMilesApplication.java
@@ -0,0 +1,13 @@
+package com.smartermiles.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMilesApplication.class, args);
+	}
+
+}
